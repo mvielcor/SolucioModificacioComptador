35cfda9a5c9875707a1295bf57c0f5512a64dcea
mviel <mviel@florida-uni.es>
2018-09-27 11:15:21 +0200

    Solució a la modificació de l'exercici

--- app/src/main/java/pmdm/mviel/debugging/MainActivity.java
+++ app/src/main/java/pmdm/mviel/debugging/MainActivity.java
@@ -16,15 +16,18 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+
 
 
 public class MainActivity extends Activity {
 
     private int comptadorDeClicks; // atribut que ens servirà per comptar els clicks que fa l'usuari
+    private int comptadorClicksRestants;
     private TextView marcador_polsacions;  // atribut que referencia el 'label' on mostrem el numero
                                            // de polsacions que ha fet l'usuari
-
+    private TextView marcador_clicks_restants;
     private final boolean DEBUG=true;
     private final String ETIQUETA="depurant";
 
@@ -42,14 +45,22 @@ public class MainActivity extends Activity {
                                                 // el contingut que tindrà MainActivity
 
         marcador_polsacions = (TextView)findViewById(R.id.marcadorPolsacions); //Ací enllacem l'objecte xml macadorPolsacions
-
+        marcador_clicks_restants = (TextView)findViewById(R.id.marcadorClicksRestants);
         if (DEBUG) Log.d(ETIQUETA,"Abans d'inicialitzar comptadorDeClics val:"+comptadorDeClicks);
         // amb un objecte JAVA de la classe TextView, per poder manipular-lo després.
         comptadorDeClicks=0; // Inicialitzem el comptador de clicks a 0
-
+        comptadorClicksRestants=5;
         marcador_polsacions.setText(""+comptadorDeClicks);  // Posem el valor 0 dins l'objecte TextView
+        marcador_clicks_restants.setText(""+comptadorClicksRestants);
     }
 
+    public void setComptadorClicksRestants(int comptadorClicksRestants) {
+        this.comptadorClicksRestants = comptadorClicksRestants;
+    }
+
+    public int getComptadorClicksRestants() {
+        return comptadorClicksRestants;
+    }
 
     //el setter de l'atribut comptadorDeClicks
     public void setComptadorDeClicks(int valor){
@@ -73,9 +84,19 @@ public class MainActivity extends Activity {
 
         int valor_comptador= this.getComptadorDeClicks();  //agafem el valor de l'atribut comptadorDeClicks
         valor_comptador++;      // l'incrementem en una unitat
+        int valor_clicks_restants = this.getComptadorClicksRestants();
+        valor_clicks_restants--;
         this.setComptadorDeClicks(valor_comptador);  //Establim el nou valor de comptadorDeClicks
+        this.setComptadorClicksRestants(valor_clicks_restants);
         marcador_polsacions.setText("" + valor_comptador);  //A l'objecte TextView, l'indiquem que mostre el nou valor de
                                                             // comptadorDeClicks
+        marcador_clicks_restants.setText(""+valor_clicks_restants);
+        if (valor_clicks_restants==0){
+            if(v.getId()==R.id.button){
+                Button boto = (Button)findViewById(R.id.button);
+                boto.setEnabled(false);
+            }
+        }
     }
 
 
